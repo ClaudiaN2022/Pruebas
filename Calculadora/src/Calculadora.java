6fb2a195574b511d6dba9a944366167cf49df761
Claudia Naveda! <claudialaboral@gmail.com>
2023-08-01 11:01:04 -0300

    Voy a la rama segundo commit

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,16 @@
+public class Calculadora {
+
+public void sumar(double numero1,double numero2){
+
+    double resultado ;
+
+    resultado = numero1+numero2;
+
+    System.out.println( " El resultado de la suma es "+resultado);
+
+
+
+}
+
+
+}
